a95e6cfe4f5f7e9cb7d1cdf3a0392b06a6803736
张奎 <bjzk_2012_zk@163.com>
2019-08-05 10:57:19 +0800

    添加按照账号查询用户方法

--- src/main/java/cn/kcyf/bsc/modular/system/dao/UserDao.java
+++ src/main/java/cn/kcyf/bsc/modular/system/dao/UserDao.java
@@ -5,6 +5,8 @@ import cn.kcyf.orm.jpa.dao.BasicDao;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserDao extends BasicDao<User, Long> {
     User findFirstByAccount(@Param("account") String account);
